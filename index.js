916dbb2e8e10fa34dca27b11d853b933b2642cfe
Nachowithpepsi <157248595+Nachowithpepsi@users.noreply.github.com>
2024-01-20 15:06:13 -0600

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["👑 I am RTX","Spotify","Youtube","SUBSCRIBE❤️","THANKS 🌇"];
+const statusMessages = ["#WeAreCostaRica","#CRP","#CostaRicaRoleplay];
 
 
 let currentIndex = 0;
